c506515d6a60e546a05b333ec74cc355c023a1bf
zhao jun <873019219@qq.com>
2023-11-14 19:48:08 +0800

    更新

--- util/vars.sh
+++ util/vars.sh
@@ -30,7 +30,7 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-REPO="${GITHUB_REPOSITORY:-btbn/ffmpeg-builds}"
+REPO="${GITHUB_REPOSITORY:-zhaojun1998/ffmpeg-builds}"
 REPO="${REPO,,}"
 REGISTRY="${REGISTRY_OVERRIDE:-ghcr.io}"
 BASE_IMAGE="${REGISTRY}/${REPO}/base:latest"
